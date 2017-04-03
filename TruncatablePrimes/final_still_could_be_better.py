4134d359201df4bdc6e3a2a18bfaa13b63a539b4
melwyn95 <melwyn95@gmail.com>
2017-04-03 11:24:48 +0530

    Optimized Truncatable Primes to run all cases... the code can still be made better... make it more human readable

---
+++ TruncatablePrimes/final_still_could_be_better.py
@@ -0,0 +1,60 @@
+import math, time
+def seive(n):
+    num = [True] * n
+    num[0] = num[1] = False
+    for i in range(2, int(math.sqrt(n)+1)):
+        if num[i]:
+            j = 2*i
+            while j < n:
+                num[j] = False
+                j += i
+    primes = []
+    for i in range(n):
+        if num[i]: primes.append(i)
+    return primes
+def bin_search(a, n, l, h):
+    if l <= h:
+        m = (l+h)/2
+        if a[m] == n:
+            return True
+        else:
+            if n < a[m]:
+                return bin_search(a, n, l, m-1)
+            else:
+                return bin_search(a, n, m+1, h)
+    return False
+def r_l_digits(n, primes, l):
+    d = []
+    while n > 0:
+        if not bin_search(primes, n, 0, l-1):
+            return False
+        d.append(n % 10)
+        n /= 10
+    return True
+def l_r_digits(n, primes, l):
+    no_d = -1
+    t = n
+    while t > 0:
+        no_d += 1
+        t /= 10
+    den = 10 ** no_d
+    while n > 0:
+        if not bin_search(primes, n, 0, l-1):
+            return False
+        n -= (n/den)*den
+        den /= 10
+    return True
+def is_trunc(n, primes, l):
+    r_l = r_l_digits(n, primes, l)
+    l_r = False
+    if r_l:
+        l_r = l_r_digits(n, primes, l)
+    return r_l and l_r
+n = int(raw_input())
+primes = seive(n)
+l = len(primes)
+s = 0
+for i in range(10, n):
+    if is_trunc(i, primes, l):
+        s += i
+print s
